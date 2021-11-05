be0d43d2774cfc6316b0fab263f40e26760cfc6e
Constantin Litvak <litvak@d-sire.com>
2021-11-05 13:02:14 +0100

    Info screen, release v.0.0.1(4)

---
+++ lib/ui/commons/about_txt.dart
@@ -0,0 +1,9 @@
+const kAboutMarkDown = '''
+Die Anwendung **„Karten für Zeitreisende“** wurde im Rahmen des Kultur Hackathons **{COD1NG DA V1NCH1} NIEDER:RHEIN:LAND 2021** erstellt.
+
+Die historischen Stadtpläne wurden von der **Archäologische Zone Köln** und dem **Amt für Liegenschaften, Vermessung und Kataster der Stadt Köln** in Koopertion digitalisiert und frei zur Verfügung gestellt. Als WebMappingService (WMS) wurden die Stadtpläne von **OK Lab Köln / OpenMaps.online** aufbereitet.
+
+Idee, Konzept und Umsetzung der Anwendung: **Constantin Litvak, Köln**.
+
+November 2021. V.0.0.1
+''';
--- lib/ui/views/notes.dart
+++ lib/ui/views/notes.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:historical_maps/ui/commons/about_txt.dart';
+import 'package:historical_maps/ui/commons/ui_helpers.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Notes'),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(UIHelper.kHorizontalSpaceMedium),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 300,
+                    child: Image.asset('assets/graphics/logo.png'),
+                  ),
+                  UIHelper.verticalSpaceMedium(),
+                  const MarkdownBody(
+                    data: kAboutMarkDown,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
